9df7d7f79ff96e90d8b25c7b0911889d4c10227a
Ilya Timofeev <timofeevila423@gmail.com>
2024-03-14 18:56:00 +0700

    Added bet validity check

--- blackjack/static/js/game.js
+++ blackjack/static/js/game.js
@@ -1,9 +1,12 @@
+var CurrentBalance = 0
+
 function updateUserBalance() {
     const url = `http://127.0.0.1:8000/api/user-balance?id=${userId}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            document.getElementById("balance").textContent = `Balance: ${data}`;
+            CurrentBalance = data
+            document.getElementById("balance").textContent = `Balance: ${CurrentBalance}`;
         })
         .catch(error => {
             console.error('Error fetching data:', error);
@@ -25,13 +28,23 @@ function resetBalance() {
 
 function Bet() {
     const amount = document.getElementById("balanceInput").value;
+
+    if (amount < 0) {
+        alert("Пожалуйста, введите корректную сумму ставки. Ставка не может быть отрицательной!");
+        return;
+    }
+    if (amount > parseFloat(CurrentBalance)) {
+        alert("Недостаточно средств на счету! Пожалуйста, введите корректную сумму ставки.");
+        return;
+    }
+    
+
     const url = `http://127.0.0.1:8000/api/start-game?id=${userId}&bet=${amount}`;
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            // После успешного завершения запроса на сброс баланса вызываем updateUserBalance()
             updateUserBalance();
         }
     };
